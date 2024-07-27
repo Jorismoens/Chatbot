dd32942d544d5f8ce98229a6e3b00ec03e5106e3
Jorismoens <72558033+Jorismoens@users.noreply.github.com>
2024-07-27 17:02:59 +0200

    Refactor: move example prompts and api response handling to seperate files

---
+++ handleToolResponse.py
@@ -0,0 +1,52 @@
+import json
+import string
+
+from openai import OpenAI
+from openai.types.chat import ChatCompletion
+
+from callWeather import get_temperature
+from getRandomN import get_random_numbers
+
+
+def handle_tool_response(client: OpenAI, completion: ChatCompletion, messages: list[dict[str, str]]) -> string:
+    tool_calls = completion.choices[0].message.tool_calls
+
+    if tool_calls:
+        tool_name = tool_calls[0].function.name
+        if tool_name == 'get_random_numbers':
+            args = json.loads(tool_calls[0].function.arguments)
+            observation = get_random_numbers(**args)
+
+            messages.append({
+                "role": "function",
+                "name": tool_name,
+                "content": observation
+            })
+
+            response_with_function_call = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages,
+            )
+
+            return response_with_function_call.choices[0].message.content
+
+        elif tool_name == 'get_temperature':
+            args = json.loads(tool_calls[0].function.arguments)
+            observation = get_temperature(**args)
+
+            messages.append({
+                "role": "function",
+                "name": tool_name,
+                "content": observation
+            })
+
+            response_with_function_call = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages,
+            )
+
+            return response_with_function_call.choices[0].message.content
+
+    else:
+        # Model did not identify a function to call, hand the response message to the user
+        return completion.choices[0].message.content
---
+++ prompts.py
@@ -0,0 +1,11 @@
+
+# Example prompts
+PROMPTS = {
+    "headsOrTails": ("To decide what to eat tonight, we want to flip a coin. At heads we'll eat pizza, at tails a salad. "
+                     "What will we eat tonight?"),
+    "trump": "Who is Donald Trump?",
+    "willItRain": "Is there a chance of rain today in Amsterdam?",
+    "tempZimbabwe": "how hot is it in Zimbabwe today?",
+    "bbqWeather": "Is today a good day for bbq'ing in Antarctica?",
+    "whichMonth": "Based on the weather in New York, what month do you think it is?"
+}
