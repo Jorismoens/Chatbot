b7ff81d7187f315939c3300dec26f09a3bef81d4
Jorismoens <72558033+Jorismoens@users.noreply.github.com>
2024-07-31 16:49:20 +0200

    test: add test for the LLM picking the get temperature tool

--- test/testOpenAICall.py
+++ test/testOpenAICall.py
@@ -1,9 +1,9 @@
 import unittest
 from unittest.mock import patch
 
-from src.callAsistant import callAsistantWithTools
-from src.tools.toolSchemas import randomNumbersTool
 from completionMock import MockMessage, MockChoice, MockCompletion, MockFunction, MockToolCall
+from src.callAsistant import callAsistantWithTools
+from src.tools.toolSchemas import randomNumbersTool, temperatureTool
 
 
 class TestOpenAICall(unittest.TestCase):
@@ -31,7 +31,8 @@ class TestOpenAICall(unittest.TestCase):
     @patch('src.handleToolResponse.get_random_numbers')
     @patch('openai.resources.chat.completions.Completions.create')
     def test_completions_api_random_number_tool(self, mock_completions, mock_random_numbers):
-        # Define the mock response using custom classes
+        # The completions model suggests a call to the get_random_numbers function, make sure that it's called with the
+        # right arguments
         response_content = ""
         mock_function = MockFunction(arguments='{"min":1,"max":6, "count": 5}', name='get_random_numbers')
         mock_tool_call = MockToolCall(function=mock_function, type='function')
@@ -50,10 +51,32 @@ class TestOpenAICall(unittest.TestCase):
         self.assertEqual(actual_response, response_content)
         mock_random_numbers.assert_called_once_with(min=1, max=6, count=5)
 
-    # Test with non defined function schema
+    @patch('src.handleToolResponse.get_temperature')
+    @patch('openai.resources.chat.completions.Completions.create')
+    def test_completions_api_random_number_tool(self, mock_completions, mock_temperature):
+        # The completions model suggests a call to the get_temperature function, make sure that it's called with the
+        # right arguments
+        response_content = ""
+        mock_function = MockFunction(arguments='{"latitude": 20.19, "longitude": 70.12}', name='get_temperature')
+        mock_tool_call = MockToolCall(function=mock_function, type='function')
+        mock_message = MockMessage(content=response_content, tool_calls=[mock_tool_call])
+        mock_choice = MockChoice(message=mock_message)
+        mock_response = MockCompletion(choices=[mock_choice])
+        mock_completions.return_value = mock_response
+
+        mock_temperature.return_value = '{"temperature": -35}'
+
+        actual_response = callAsistantWithTools(tools=[temperatureTool], prompt="How hot is it today in Samarkand?",
+                                                role="You're a helpful assistant")
+
+        mock_completions.assert_called()
+
+        self.assertEqual(actual_response, response_content)
+        mock_temperature.assert_called_once_with(latitude=20.19, longitude=70.12)
+
     @patch('openai.resources.chat.completions.Completions.create')
     def test_completions_api_undefined_function(self, mock_completions):
-        # Define the mock response using custom classes
+        # Test with non defined function schema
         response_content = "call this non-existing function"
         mock_function = MockFunction(arguments=[], name='non-existing_function')
         mock_tool_call = MockToolCall(function=mock_function, type='function')
@@ -68,5 +91,3 @@ class TestOpenAICall(unittest.TestCase):
         mock_completions.assert_called()
 
         self.assertEqual(actual_response, "error: function call defined by model does not exist")
-
-
